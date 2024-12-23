5f26c7bfd2fac378ca160c546ca8545108cb2d9c
Matan Albert <matan.albert@gmail.com>
2024-12-23 21:24:05 +0000

    started wish.cpp

---
+++ project2/processes-shell/wish.cpp
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+// argc is the number of command line arguments, argv is the array of command line arguments
+// ie argv[0] is ./wish, argv[1] is the batch file, only for batch mode
+
+int main(int argc, char *argv[]) {
+    
+}
